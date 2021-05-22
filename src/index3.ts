e5225fa8e7f9228d9f1bd89502a1c00553df7878
azu <azuciao@gmail.com>
2021-05-22 15:00:20 +0900

    update

--- src/index3.ts
+++ src/index3.ts
@@ -3,5 +3,6 @@ import remarkGfm from "remark-gfm";
 import remarkParse from "remark-parse";
 
 const remark = unified().use(remarkParse).use(remarkGfm);
-const ast = remark.parse(`http&#x3A;//user:password@host:port/path?key=value#fragment`);
+const ast = remark.parse(`- > a block quote in a list
+    with https://example.com a URL`);
 console.log(JSON.stringify(ast, null, 4));
--- src/workaround.ts
+++ src/workaround.ts
@@ -1,7 +1,10 @@
 import unified from "unified";
+// @ts-ignore
+import autolinkLiteral from "mdast-util-gfm-autolink-literal/from-markdown";
+// disable autolink transforms
+autolinkLiteral.transforms = [];
 import remarkGfm from "remark-gfm";
 import remarkParse from "remark-parse";
-
 const remark = unified().use(remarkParse).use(remarkGfm);
 const ast = remark.parse(`http&#x3A;//user:password@host:port/path?key=value#fragment`);
 console.log(JSON.stringify(ast, null, 4));
